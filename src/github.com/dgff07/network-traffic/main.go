49e0b82a8f8363e8e63a12bea4d7c878fbe20d7c
Diogo Fernandes <d.fernandes@kigroup.de>
2023-09-01 10:48:12 +0100

    change packages names

--- src/github.com/dgff07/network-traffic/main.go
+++ src/github.com/dgff07/network-traffic/main.go
@@ -5,7 +5,7 @@ import (
 	"os"
 	"strconv"
 
-	network "github.com/dgff07/network-traffic/src/github.com/dgff07/network-traffic/network/channel"
+	"github.com/dgff07/network-traffic/src/github.com/dgff07/network-traffic/network/channel"
 )
 
 const (
@@ -46,7 +46,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	netService := network.BuildNetworkService(bufferSize)
+	netService := channel.BuildNetworkService(bufferSize)
 
 	netService.InitChannelReader()
 
--- src/github.com/dgff07/network-traffic/network/mutex/network.go
+++ src/github.com/dgff07/network-traffic/network/mutex/network.go
@@ -1,4 +1,4 @@
-package network
+package mutex
 
 import (
 	"fmt"
